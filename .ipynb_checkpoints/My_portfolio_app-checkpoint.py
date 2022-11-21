14bad02f2c9edc74dab2cb28481448ccf44d5c37
marta <martalbarbosa@gmail.com>
2022-11-21 16:06:17 +0000

    Update code

--- .ipynb_checkpoints/My_portfolio_app-checkpoint.py
+++ .ipynb_checkpoints/My_portfolio_app-checkpoint.py
@@ -4,7 +4,54 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
-st.title("My portfolio app")
+# import nltk
+# nltk.download('punkt')
+# from wordcloud import WordCloud
 
-page_names=['Home', 'Hi, I\'m Marta!', 'Portfolio']
-page=st.radio('Please choose the page:', page_names)
+# import nltk
+# nltk.download()
+# from wordcloud import WordCloud
+
+from streamlit_option_menu import option_menu
+from PIL import Image
+
+
+st.set_page_config(page_title="Marta_Barbosa_app", layout="wide", menu_items=None)
+
+with st.sidebar:
+        selected = option_menu("Main Menu", ['Home', 'Hi, I\'m Marta!', 'Portfolio'], 
+            icons=['house'], menu_icon="cast", default_index=1)
+        
+        #Source: https://discuss.streamlit.io/t/streamlit-option-menu-is-a-simple-streamlit-component-that-allows-users-to-select-a-single-item-from-a-list-of-options-in-a-menu/20514
+        
+if selected == 'Home':
+        st.title("My portfolio app")
+        st.write('some text')
+
+if selected == 'Hi, I\'m Marta!':
+        st.title("Hi, I\'m Marta!")
+        
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write('some text about me')            
+        with col2:
+            image1 = Image.open('./Images/Capturar1.JPG')
+            st.image(image1, width = 500)
+            
+        col1, col2 = st.columns(2)
+        with col1:
+            st.title("Soft-Skills")
+            
+            text = 'curious, curious, curious, responsible, responsible, pro-active, pro-active, pro-active'
+            wordcloud = WordCloud(width=480, height=480, max_font_size=200, min_font_size=10)
+            wordcloud.generate_from_text(text)
+            plt.figure()
+            plt.imshow(wordcloud, interpolation="bilinear")
+            plt.margins(x=0, y=0)
+            plt.show()
+            
+        with col2:
+            st.title("Hard-Skills")      
+
+if selected == 'Portfolio':
+        st.title('This is my Portfolio')
