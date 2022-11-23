aa67847808f9d81a3f99c5b014f9591734b1c384
marta <martalbarbosa@gmail.com>
2022-11-23 12:01:28 +0000

    ok

--- My_portfolio_app.py
+++ My_portfolio_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
+# import plotly.express as px
 
 # import nltk
 # nltk.download('punkt')
